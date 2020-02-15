66c2a7341afe5e07dd82b5671d6cc1ee523d41cf
akashche <akashche@redhat.com>
2020-02-15 22:40:55 +0000

    ui tree navigation

--- app/Main.hs
+++ app/Main.hs
@@ -18,23 +18,26 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE Strict #-}
+{-# LANGUAGE StrictData #-}
 
 module Main where
 
 import Prelude ()
 import VtUtils.Prelude
+import qualified Data.Vector as Vector
+import qualified Data.HashMap.Strict as HashMap
 import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 import Graphics.UI.FLTK.LowLevel.Fl_Enumerations (Boxtype(DownBox))
 import Graphics.UI.FLTK.LowLevel.FLTKHS
-        ( Group, Ref, Tree
-        , add, begin, doubleWindowNew, getH, getW, groupNew, end
-        , setBox, setCallback, setResizable, setShowroot, showWidget, sizeRange
-        , tileNew, treeNew, toRectangle, toSize
+        ( Group, Rectangle, Ref, Size, TextBuffer, Tree, TreeReasonType(TreeReasonSelected)
+        , add, appendToBuffer, boxNew, doubleWindowNew, getCallbackItem, getLabel, getCallbackReason
+        , getParent, groupNew, hide, end
+        , setBox, setBuffer, setCallback, setResizable, setShowroot, showWidget, sizeRange
+        , textDisplayNew, textBufferNew, tileNew, treeNew, toRectangle, toSize
         )
 
-treeElements:: [Text]
-treeElements =
+treeElements :: Vector Text
+treeElements = fromList
     [ "Destination/Server"
     , "Destination/Input"
     , "Destination/Output"
@@ -46,59 +49,106 @@ treeElements =
     , "About"
     ]
 
-treeCallback :: Ref Tree -> IO ()
-treeCallback _tree' = do
+createGroup :: Rectangle -> Text -> IO (Ref Group)
+createGroup rect name = do
+        gr <- groupNew rect (Just name)
+        setBox gr DownBox
+        _ <- boxNew contentRect (Just name)
+        end gr
+        hide gr
+        return gr
+
+treeGroups :: Vector Text -> Rectangle -> IO (HashMap Text (Ref Group))
+treeGroups names rect = do
+    groups <- Vector.mapM fun names
+    let pairs = Vector.zip names groups
+    let map = HashMap.fromList (toList pairs)
+    return map
+    where
+        fun nm = createGroup rect nm
+
+treeCallback :: Ref TextBuffer -> HashMap Text (Ref Group) -> Ref Tree -> IO ()
+treeCallback tbuff map tree = do
+    (Just item) <- getCallbackItem tree
+    label <- getLabel item
+    (Just parent) <- getParent item
+    parentLabel <- getLabel parent
+    let path = if "ROOT" == parentLabel then label else parentLabel <> "/" <> label
+    reason <- getCallbackReason tree
+    when (TreeReasonSelected == reason) $ do
+        forM_ (HashMap.elems map) $ \gr -> do
+            hide gr
+            return ()
+        case lookup path map of
+            Just gr -> do
+                _ <- showWidget gr
+                return ()
+            Nothing -> return ()
+        appendToBuffer tbuff path
+        appendToBuffer tbuff "\n"
     return ()
---  (Just item') <- getCallbackItem tree'
---  reason' <- getCallbackReason tree'
---  label' <- pack <$> getLabel item'
---  case reason' of
---    TreeReasonSelected -> do
---      (Just path') <- itemPathname tree' item'
---      putStrLn $ "TreeCallback: Item selected =" <> label' <> "Full pathname=" <> (unpack path') <> "\n"
---    TreeReasonDeselected ->
---      print $ "TreeCallback: Item deselected =" ++ (unpack label') ++ "\n"
---    TreeReasonOpened ->
---      print $ "TreeCallback: Item opened1 =" ++ (unpack label') ++ "\n"
---    TreeReasonClosed ->
---      print $ "TreeCallback: Item closed =" ++ (unpack label') ++ "\n"
---    _ -> print ("" :: String)
+
+windowWidth :: Int
+windowWidth = 640
+
+windowHeight :: Int
+windowHeight = 480
+
+treeWidth :: Int
+treeWidth = 200
+
+debugHeight :: Int
+debugHeight = 200
+
+windowSize :: Size
+windowSize = toSize (windowWidth, windowHeight)
+
+tileRect :: Rectangle
+tileRect = toRectangle (0, 0, windowWidth, windowHeight - debugHeight)
+
+treeRect :: Rectangle
+treeRect = toRectangle (0, 0, treeWidth, windowHeight - debugHeight)
+
+contentRect :: Rectangle
+contentRect = toRectangle (treeWidth, 0, windowWidth - treeWidth, windowHeight - debugHeight)
+
+debugRect :: Rectangle
+debugRect = toRectangle (0, windowHeight - debugHeight, windowWidth, debugHeight)
 
 main :: IO ()
 main = do
-    window <- doubleWindowNew (toSize (640, 480)) Nothing (Just "Simple Tree")
+    window <- doubleWindowNew windowSize Nothing (Just "Proxy Tester")
     sizeRange window 1 1
-    setResizable window (Just window)
-    windowWidth <- getW window
-    windowHeight <- getH window
-    begin window
+    setResizable window (Nothing :: Maybe (Ref Group))
 
-    tile <- tileNew (toRectangle (0, 0, windowWidth, windowHeight)) Nothing
-    begin tile
+    disp <- textDisplayNew debugRect Nothing
+    tbuff <- textBufferNew Nothing Nothing
+    appendToBuffer tbuff "Debug:\n"
+    setBuffer disp (Just tbuff)
+    end disp
 
-    tree <- treeNew (toRectangle (0, 0, 200, windowHeight)) Nothing
+    tile <- tileNew tileRect Nothing
+
+    tree <- treeNew treeRect Nothing
     setShowroot tree False
+    end tree
+    setResizable window (Just tree)
+
+    map <- treeGroups treeElements contentRect
     forM_ (treeElements) $ \el -> do
-      _ <- add tree el
-      return ()
-    setCallback tree treeCallback
+        _ <- add tree el
+        return ()
+    setCallback tree (treeCallback tbuff map)
 
-    rightGroup <- groupNew (toRectangle (200, 0, windowWidth - 200, windowHeight)) Nothing
-    setBox rightGroup DownBox
-    rightGroupWidth <- getW rightGroup
-    setResizable rightGroup (Nothing :: Maybe (Ref Group))
-    begin rightGroup
+    case lookup "About" map of
+        Just gr -> do
+            _ <- showWidget gr
+            return ()
+        Nothing -> return ()
 
-    contentGroup <- groupNew (toRectangle (200, 0, rightGroupWidth, windowHeight - 40)) Nothing
-    setBox contentGroup DownBox
-    setResizable rightGroup (Just contentGroup)
-
-    buttonsGroup <- groupNew (toRectangle (200, windowHeight - 40, rightGroupWidth, 40)) Nothing
-    setBox buttonsGroup DownBox
-
-    end rightGroup
     end tile
     end window
+
     showWidget window
     _ <- FL.run
     return ()
